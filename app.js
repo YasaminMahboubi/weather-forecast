bd4a0cb622f3f42d1de96cbd06ffae2a022370d4
Yasamin Mahboubi <103999618+YasaminMahboubi@users.noreply.github.com>
2023-12-21 10:24:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,39 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.set('view engine' , 'ejs');
-app.use('/public',express.static("public"));
+app.use('/public' , express.static(path.join(__dirname , "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-app.get('/',  (req, res) => { 
-    fetchCity().then(data => {
-        buildHTML(data).then(htmlInfo => {
-          res.render('index', { htmlData: htmlInfo });
-        })
-    });
+const dayOfTheWeek = (loopCounter) => {
+    let date = new Date;
+    let today = date.getDay();
+
+    if(today+loopCounter > 6){
+        return days[today+loopCounter - 7];
+    }
+    else if(loopCounter == 0){
+        return "today";
+    }
+    else{
+        return days[today+loopCounter];
+    }
+}
+
+app.get('/',  async (req, res) => { 
+    try{
+        let cityName = await fetchCity();
+        let htmlInfo = await buildHTML(cityName);
+        res.render('index', { htmlData: htmlInfo });
+    }
+    catch(error){
+        console.error("" + error);
+        res.sendStatus(404);
+    }
 }) 
 
 async function buildHTML(data){
@@ -25,7 +45,7 @@ async function buildHTML(data){
             }
             let todayArray = [];
             for(let i=0;i<6;i++){
-                let clock = Number(weatherRes.list[i]['dt_txt'].split(' ')[1].split(':')[0]) < 12 ?                     
+                let clock   =   Number(weatherRes.list[i]['dt_txt'].split(' ')[1].split(':')[0]) < 12 ?                     
                                 Number(weatherRes.list[i]['dt_txt'].split(' ')[1].split(':')[0])  + ":00 AM" : 
                                 Number(weatherRes.list[i]['dt_txt'].split(' ')[1].split(':')[0]) + "00: PM";
                 todayArray[i] = [ clock , 
@@ -33,22 +53,9 @@ async function buildHTML(data){
                                 Math.floor(weatherRes.list[i].main.temp - 273.15)
                                 ];
             }
-
-            let d = new Date;
-            let today = d.getDay();
             let sevenDayArray = [];
             for(i=0;i<5;i++){
-                let day = '';
-                let dayCounter = 0;
-                if(today+i > 6){
-                    dayCounter = today+i - 7;
-                    day = days[dayCounter]
-                }else{
-                    day = days[today+i]
-                }
-                if(i == 0){
-                    day = "today";
-                }
+                let day = dayOfTheWeek(i);
                 sevenDayArray[i] = [ day , 
                                     `public/images/${weatherRes.list[i].weather[0].icon}.png`,
                                     Math.floor(weatherRes.list[i].main.temp_min - 273) + '/' + Math.ceil(weatherRes.list[i].main.temp_max - 273.15)
@@ -61,7 +68,6 @@ async function buildHTML(data){
                 icon: `public/images/${weatherRes.list[0].weather[0].icon}.png`,
                 humidity: weatherRes.list[0].main.humidity,
                 desc: weatherRes.list[0].weather[0].description,
-
                 forecastToday: todayArray,
                 sevenDay: sevenDayArray
             };
@@ -70,27 +76,36 @@ async function buildHTML(data){
 
         } catch (error) {
           console.error('Error in buildHTML:', error);
-          throw error; 
+          res.sendStatus(404);
         }
 }
 
 async function getIp() {
-    const response = await axios.get('https://api.ipify.org?format=json');
-    const publicIp = await response.data.ip;
-    return publicIp;
+    try{
+        const response = await axios.get('https://api.ipify.org?format=json');
+        const publicIp = await response.data.ip;
+        return publicIp;
+    }
+    catch(err){
+        console.error("error in getIp is" + err);
+        res.sendStatus(404);
+    }
+    
 }
 
 async function fetchCity() {
     try {
-        let publicIp = await getIp();
+        let publicIp   = await getIp();
         const response = await fetch(`https://www.iplocate.io/api/lookup/${publicIp}/json?apikey=17e28da65f14ef679cb51a76a6edeefd`);
         const result = await response.json();
         return result.city; 
     } 
     catch (err) {
-        console.error(err);
+        console.error("error in fetchCity is" + err);
+        res.sendStatus(404);
     }
 }
+
 async function fetchWeather(city) {
     try{
         const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ce2100858e026bf52e7c89aa4154e525`)
@@ -102,12 +117,14 @@ async function fetchWeather(city) {
         }
     }
     catch(err){
-        console.error(err);
+        console.error("error in fetchWeather is" + err);
+        res.sendStatus(404);
     }
 }
 
-app.post('/city' , (req,res) => {
-    buildHTML(req.body.searchCity).then(htmlInfo => {
+app.post('/city' , async (req,res) => {
+    try{
+        let htmlInfo = await buildHTML(req.body.searchCity);
         if(htmlInfo == 404){
             fetchCity().then(data => {
                 buildHTML(data).then(htmlInfo => {
@@ -119,7 +136,11 @@ app.post('/city' , (req,res) => {
             htmlInfo.error = false;
             res.render('index', { htmlData: htmlInfo });
         }
-      })
+    }
+    catch(err){
+        console.error("error in /city is "+ err);
+        res.sendStatus(404);
+    }   
 })
 
 app.listen(3000 , () => {
